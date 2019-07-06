dc20daf12aeb6883b0cdd9ec3342f4c38efe67da
Niral J Shah <51793882+niraljshah@users.noreply.github.com>
2019-07-06 18:39:45 +1000

    Add files via upload
    Modified

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 from setuptools import setup
 
 setup(
     name='BasicAnalysis',
-    version='0.0.1',
+    version='0.0.2',
     description='It is a fast tool to see the best supervised learning method for your dataset',
     license='MIT',
     packages=['BasicAnalysis'],
